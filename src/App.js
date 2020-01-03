76d9562af932d669002de12a5c4502a7fd9e598e
Luis Carlos Mendez Rodas <luiiscmr@gmail.com>
2020-01-03 00:17:35 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import './App.css';
+import SideBar from './components/SideBar/SideBar'
+import { makeStyles, useTheme, createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import red from '@material-ui/core/colors/red';
+import Typography from '@material-ui/core/Typography';
+
+const darkTheme = createMuiTheme({
+  palette: {
+      // Switching the dark mode on is a single property value change.
+      type: 'dark',
+      primary: red,
+  },
+});
+
+const useStyles = makeStyles(theme => ({
+  toolbar: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    padding: theme.spacing(0, 1),
+    color: 'white',
+    ...theme.mixins.toolbar,
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+  },
+}));
+
+function App() {
+  const classes = useStyles();
+  return (
+    <div>
+      <ThemeProvider theme={darkTheme}>
+        <SideBar />
+      </ThemeProvider>
+    </div>
+  );
+}
+
+export default App;
